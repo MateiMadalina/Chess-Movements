2ccdb52a67c2456461763dd6d23eae8631c2d584
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-07-17 16:40:43 +0300

    MovementEngine

--- src/main/java/com/codecool/chessmovements/logic/MovementEngine.java
+++ src/main/java/com/codecool/chessmovements/logic/MovementEngine.java
@@ -1,15 +1,30 @@
 package com.codecool.chessmovements.logic;
 
 import com.codecool.chessmovements.data.Position;
+import com.codecool.chessmovements.logic.generator.MovementGenerator;
 
 import java.util.List;
 
 public class MovementEngine {
 
+    private final MovementGenerator generator;
+    private final ChessBoardBoundaries boardBoundaries;
+
+    public MovementEngine(MovementGenerator generator, ChessBoardBoundaries boardBoundaries) {
+        this.generator = generator;
+        this.boardBoundaries = boardBoundaries;
+    }
+
     public List<Position> generate(String type, Position current) {
+     List<Position> possibleMoves = generator.generate(current);
+     List<Position> finalMoves = null;
 
-        // To be implemented
+        for (int i = 0; i < possibleMoves.size(); i++) {
+            if(boardBoundaries.fits(possibleMoves.get(i))){
+                finalMoves.add(possibleMoves.get(i));
+            }
+        }
 
-        return null;
+        return finalMoves;
     }
 }
---
+++ src/main/java/com/codecool/chessmovements/logic/generator/PawnGenerator.java
@@ -0,0 +1,4 @@
+package com.codecool.chessmovements.logic.generator;
+
+public class PawnGenerator {
+}
